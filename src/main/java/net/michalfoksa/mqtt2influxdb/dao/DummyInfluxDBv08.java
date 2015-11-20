6f3ff3175260774a7f8d1a07a3efb62a244b029a
Michal Foksa <mail@michalfoksa.net>
2015-11-20 13:36:47 +0100

    Warning messages added that actually no action is being performed.

--- src/main/java/net/michalfoksa/mqtt2influxdb/dao/DummyInfluxDBv08.java
+++ src/main/java/net/michalfoksa/mqtt2influxdb/dao/DummyInfluxDBv08.java
@@ -6,8 +6,12 @@ import org.influxdb.v08.InfluxDB;
 import org.influxdb.v08.dto.Pong;
 import org.influxdb.v08.dto.Serie;
 import org.influxdb.v08.impl.InfluxDBImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DummyInfluxDBv08 extends InfluxDBv08 {
+
+    private static Logger log = LoggerFactory.getLogger(DummyInfluxDBv08.class) ;
 
     public DummyInfluxDBv08(String uri, String username, String password) {
         super(uri, username, password);
@@ -20,6 +24,8 @@ public class DummyInfluxDBv08 extends InfluxDBv08 {
 
             @Override
             public Pong ping() {
+                log.warn("This is dummy implementation. Connection to database IS NOT really opened.");
+
                 Pong pong = new Pong();
                 pong.setStatus("ok");
                 return pong;
@@ -28,6 +34,7 @@ public class DummyInfluxDBv08 extends InfluxDBv08 {
             @Override
             public void write(String database, TimeUnit precision,
                     Serie... series) {
+                log.warn("This is dummy implementation. NO insert into database is really performed.");
                 // Do nothing
                 //super.write(database, precision, series);
             }
--- src/main/java/net/michalfoksa/mqtt2influxdb/dao/DummyInfluxDBv09.java
+++ src/main/java/net/michalfoksa/mqtt2influxdb/dao/DummyInfluxDBv09.java
@@ -5,10 +5,14 @@ import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Pong;
 import org.influxdb.impl.InfluxDBImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DummyInfluxDBv09 extends InfluxDBv09 {
 
     public static final String MOCK_VERSION = "mock_0.9.x";
+
+    private static Logger log = LoggerFactory.getLogger(DummyInfluxDBv09.class) ;
 
     public DummyInfluxDBv09(String uri, String username, String password) {
         super(uri, username, password);
@@ -21,6 +25,8 @@ public class DummyInfluxDBv09 extends InfluxDBv09 {
 
             @Override
             public Pong ping() {
+                log.warn("This is dummy implementation. Connection to database IS NOT really opened.");
+
                 Pong pong = new Pong();
                 pong.setVersion(MOCK_VERSION);
                 return pong;
@@ -29,12 +35,14 @@ public class DummyInfluxDBv09 extends InfluxDBv09 {
             @Override
             public void write(String database, String retentionPolicy,
                     Point point) {
+                log.warn("This is dummy implementation. NO insert into database is really performed.");
                 // Do nothing
                 //super.write(database, retentionPolicy, point);
             }
 
             @Override
             public void write(BatchPoints batchPoints) {
+                log.warn("This is dummy implementation. NO insert into database is really performed.");
                 // Do nothing
                 //super.write(batchPoints);
             }
