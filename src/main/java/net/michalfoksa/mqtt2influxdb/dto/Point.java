09f8e08efe147f87ad967474ced68966c0bacdc4
Michal Foksa <mail@michalfoksa.net>
2015-11-20 19:50:57 +0100

    Getters to read tags, time and precision added. Builder sets time to current time by default. lineProtocol() method dropped.

--- src/main/java/net/michalfoksa/mqtt2influxdb/dto/Point.java
+++ src/main/java/net/michalfoksa/mqtt2influxdb/dto/Point.java
@@ -1,8 +1,10 @@
 package net.michalfoksa.mqtt2influxdb.dto;
 
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
@@ -11,9 +13,9 @@ import com.google.common.collect.Ordering;
 
 /**
  * Representation of a InfluxDB database Point.
- * 
+ *
  * @author stefan.majer [at] gmail.com
- * 
+ *
  */
 public class Point {
     private String measurement;
@@ -47,6 +49,9 @@ public class Point {
      *
      */
     public static class Builder {
+
+        private static Logger log = LoggerFactory.getLogger(Builder.class) ;
+
         private final String measurement;
         private final Map<String, String> tags = Maps.newTreeMap(Ordering.natural());
         private Long time;
@@ -70,7 +75,19 @@ public class Point {
          * @return the Builder instance.
          */
         public Builder tag(final String tagName, final String value) {
-            this.tags.put(tagName, value);
+            tags.put(tagName, value);
+            return this;
+        }
+
+        /**
+         * Add a Map of tags to add to this point.
+         *
+         * @param tagsToAdd
+         *            the Map of tags to add
+         * @return the Builder instance.
+         */
+        public Builder tag(final Map<String, String> tagsToAdd) {
+            tags.putAll(tagsToAdd);
             return this;
         }
 
@@ -84,7 +101,19 @@ public class Point {
          * @return the Builder instance.
          */
         public Builder field(final String field, final Object value) {
-            this.fields.put(field, value);
+            fields.put(field, value);
+            return this;
+        }
+
+        /**
+         * Add a Map of fields to this point.
+         *
+         * @param fieldsToAdd
+         *            the fields to add
+         * @return the Builder instance.
+         */
+        public Builder fields(final Map<String, Object> fieldsToAdd) {
+            fields.putAll(fieldsToAdd);
             return this;
         }
 
@@ -96,9 +125,9 @@ public class Point {
          * @return the Builder instance.
          */
         public Builder time(final long timeToSet, final TimeUnit precisionToSet) {
-            // FIXME convert to millis.
-            this.precision = precisionToSet;
-            this.time = timeToSet;
+            Preconditions.checkNotNull(precisionToSet, "Precision must be not null!");
+            time = timeToSet;
+            precision = precisionToSet;
             return this;
         }
 
@@ -109,14 +138,21 @@ public class Point {
          */
         public Point build() {
             Preconditions.checkArgument(
-                    !Strings.isNullOrEmpty(this.measurement),
+                    !Strings.isNullOrEmpty(measurement),
                     "Point name must not be null or empty.");
             Point point = new Point();
-            point.setFields(this.fields);
-            point.setMeasurement(this.measurement);
-            point.setPrecision(this.precision);
-            point.setTags(this.tags);
-            point.setTime(this.time);
+            point.setFields(fields);
+            point.setMeasurement(measurement);
+            if (time != null) {
+                point.setTime(time);
+                point.setPrecision(precision);
+            } else {
+                point.setTime(System.currentTimeMillis());
+                point.setPrecision(TimeUnit.MILLISECONDS);
+            }
+            point.setTags(tags);
+
+            log.debug(point.toString());
             return point;
         }
     }
@@ -128,7 +164,11 @@ public class Point {
     void setMeasurement(final String measurement) {
         this.measurement = measurement;
     }
-    
+
+    /***
+     *
+     * @return measurement
+     */
     public String getMeasurement() {
         return measurement;
     }
@@ -142,6 +182,13 @@ public class Point {
     }
 
     /**
+     * @return time
+     */
+    public Long getTime() {
+        return time;
+    }
+
+    /**
      * @param tags
      *            the tags to set
      */
@@ -152,8 +199,8 @@ public class Point {
     /**
      * @return the tags
      */
-    Map<String, String> getTags() {
-        return this.tags;
+    public Map<String, String> getTags() {
+        return tags;
     }
 
     /**
@@ -164,6 +211,13 @@ public class Point {
         this.precision = precision;
     }
 
+    /***
+     * @return precision
+     */
+    public TimeUnit getPrecision() {
+        return precision;
+    }
+
     /**
      * @param fields
      *            the fields to set
@@ -171,7 +225,10 @@ public class Point {
     void setFields(final Map<String, Object> fields) {
         this.fields = fields;
     }
-    
+
+    /***
+     * @return fields
+     */
     public Map<String, Object> getFields() {
         return fields;
     }
@@ -183,51 +240,16 @@ public class Point {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Point [name=");
-        builder.append(this.measurement);
+        builder.append(measurement);
         builder.append(", time=");
-        builder.append(this.time);
+        builder.append(time);
         builder.append(", tags=");
-        builder.append(this.tags);
+        builder.append(tags);
         builder.append(", precision=");
-        builder.append(this.precision);
+        builder.append(precision);
         builder.append(", fields=");
-        builder.append(this.fields);
+        builder.append(fields);
         builder.append("]");
         return builder.toString();
-    }
-
-    // measurement[,tag=value,tag2=value2...] field=value[,field2=value2...] [unixnano]
-
-    /**
-     * calculate the lineprotocol entry for a single Point.
-     * 
-     * Documentation is WIP : https://github.com/influxdb/influxdb/pull/2997
-     *
-     * @return the String without newLine.
-     */
-    public String lineProtocol() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.measurement);
-        for (Entry<String, String> tag : this.tags.entrySet()) {
-            sb.append(",");
-            sb.append(tag.getKey()).append("=").append(tag.getValue());
-        }
-        sb.append(" ");
-        int fieldCount = this.fields.size();
-        int loops = 0;
-        for (Entry<String, Object> field : this.fields.entrySet()) {
-            loops++;
-            sb.append(field.getKey()).append("=").append(field.getValue());
-            if (loops < fieldCount) {
-                sb.append(",");
-            }
-        }
-        if (null == this.time) {
-            this.time = System.currentTimeMillis();
-        }
-        if (this.time != null) {
-            sb.append(" ").append(TimeUnit.NANOSECONDS.convert(this.time, this.precision));
-        }
-        return sb.toString();
     }
 }
